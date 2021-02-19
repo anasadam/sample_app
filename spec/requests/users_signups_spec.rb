37c5f24e9febc22bd773d221e67316600b8ee4df
anasadam <iman.hamaad.ali@gmail.com>
2021-02-19 20:36:37 +0200

    modifying signup request spec

--- spec/requests/users_signups_spec.rb
+++ spec/requests/users_signups_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "UsersSignups", type: :request do
-  describe "GET /users_signups" do
+  
+  describe "GET /signup" do
     it "works! (now write some real specs)" do
-      get users_signups_path
+      get signup_path
       expect(response).to have_http_status(200)
     end
   end
+
+  
 end
